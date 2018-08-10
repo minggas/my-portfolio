7991badb25b7a82bc6583bf5b52d8c3c7ee17119
minggas <fasfora@gmail.com>
2018-08-09 22:29:22 -0300

    styled the contact page

--- contact.php
+++ contact.php
@@ -1,4 +1,27 @@
 <html>
+  <head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="canonical" href="https://minggas.website">
+    <link rel="manifest" href="manifest.json">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <meta name="description" content="I'm a Front End Developer from Brazil. Here you can find informations about me and my projects.">
+    <title>Minggas Webpage | Front End Developer</title>
+    <meta property="og:title" content="Minggas Webpage | Front End Developer">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://minggas.website">
+    <meta property="og:image" content="http://www.adhamdannaway.com/wp-content/themes/dannaway/images/portfolio/campaign-monitor-4/main.jpg">
+    <meta property="og:image:width" content="590">
+    <meta property="og:image:height" content="440">
+    <meta property="og:site_name" content="Minggas Webpage">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:description" content="I'm a Front End Developer from Brazil. Here you can find informations about me and my projects.">
+    <meta name="twitter:title" content="Minggas Webpage | Front End Developer">
+    <meta name="twitter:site" content="@minggasweb">
+    <meta name="twitter:creator" content="@minggasweb">
+    <link rel="stylesheet" href="css/main1.css">
+  </head>
 <body>
 <?php
 // define variables and set to empty values
@@ -28,10 +51,21 @@ $msg = wordwrap($msg,70);
 // send email
 mail("fasfora@gmail.com","Contact from Webpage",$msg);
 ?>
-
-Welcome <?php echo $name; ?><br>
-Your email address is: <?php echo $email; ?><br>
-Your message is: <?php echo $msg; ?>
-
+<nav class="nav-bar" id="navbar">
+        <div class="nav-logo">
+            <span>MINGGAS</span>
+        </div>
+        <div class="nav-toggler">
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+        </div>
+        
+    </nav>
+    <section class="tagline" style="height:100vh;">
+      <h2>Thanks for your contact, <?php echo $name; ?>!</h2>
+      <br/>
+      <span>Back to the <a href="https://minggas.website" style="color:#64fcf2">site</a></span>
+    </section>        
 </body>
 </html>
